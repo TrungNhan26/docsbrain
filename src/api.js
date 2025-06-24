d13690a836cd55726490c5f01aa40b166c0b9782
TrungNhan26 <thaohiennhan2012@gmail.com>
2025-06-24 11:33:16 +0700

    update api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Thay đổi baseURL nếu backend của bạn ở nơi khác
+  baseURL: 'https://docsbrain.id.vn', // Thay đổi baseURL nếu backend của bạn ở nơi khác
   headers: {
     'Content-Type': 'application/json'
   }
